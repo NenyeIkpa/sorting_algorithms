a662edac1ff84841c6a278e7e4e93289598596da
NenyeIkpa <nenyeikpa@gmail.com>
2023-10-23 10:58:42 +0000

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - performs bubble sort algorithm on given array
+ *
+ * @array: array to be sorted
+ * @size: size of array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int i = 0, val, flag;
+
+	if (size < 2)
+		return;
+	while ((size_t)i < size)
+	{
+		val = 0;
+		flag = 0;
+		while ((size_t)i < size - 1)
+		{
+			if (array[i] > array[i + 1])
+			{
+				flag = 1;
+				val = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = val;
+				print_array(array, size);
+				break;
+			}
+			i++;
+		}
+		if (flag == 0)
+			return;
+		i = 0;
+	}
+}
